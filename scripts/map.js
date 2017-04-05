03816f94fc562733d43e26dd46bc610e8719e3b4
Rawrjay <rudydesigns@gmail.com>
2017-04-05 13:49:05 -0700

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -125,11 +125,11 @@ $(window).on('load', function() {
           point['Icon Color']
         );
 
-            if (point.Latitude !== '' && point.Longitude !== '') {
+                  if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          point['Contact'] + ':' + point['Mobile'] + '<br>' +
-          'Office' + ':' + point['Office'] +
+          point['Contact'] + '&nbsp;' + point['Mobile'] + '<br>' +
+          'Office' + ':' + '&nbsp;' + point['Office'] + '<br>' +
           point['Description']);
 
         if (layers !== undefined && layers.length !== 1) {
